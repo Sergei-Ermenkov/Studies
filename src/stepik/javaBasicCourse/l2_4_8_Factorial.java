9e19a062a6d9c443465561f9f11880d11e791292
Sergei-Ermenkov <Sergei-Ermenkov@users.noreply.github.com>
2017-07-15 22:01:46 +0300

    Added two new solutions to 2.4.8 on the Java Basic Course.
    Using functional programming and recursive algorithm.

--- src/stepik/javaBasicCourse/l2_4_8_Factorial.java
+++ src/stepik/javaBasicCourse/l2_4_8_Factorial.java
@@ -1,6 +1,7 @@
 package stepik.javaBasicCourse;
 
 import java.math.BigInteger;
+import java.util.stream.IntStream;
 
 /**
  * Решение задания 2.4.8 по курсу "Java.Базовый курс" на stepik.org
@@ -25,6 +26,10 @@ public class l2_4_8_Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(1));
         System.out.println(factorial(3));
+        System.out.println(factorialRecurs(1));
+        System.out.println(factorialRecurs(3));
+        System.out.println(factorialStream(1));
+        System.out.println(factorialStream(3));
     }
 
     /**
@@ -34,10 +39,35 @@ public class l2_4_8_Factorial {
      * @return factorial of <code>value</code>
      */
     public static BigInteger factorial(int value) {
-        BigInteger bInt = BigInteger.valueOf(1);
+        BigInteger bInt = BigInteger.ONE;
         for (int i = 1; i <= value; i++) {
             bInt = bInt.multiply(BigInteger.valueOf(i));
         }
         return bInt;
+    }
+
+    /**
+     * Calculates factorial of given <code>value</code>.
+     * <p>Uses a recursive algorithm.
+     *
+     * @param value positive number
+     * @return factorial of <code>value</code>
+     */
+    public static BigInteger factorialRecurs(int value) {
+        if (value == 1) return BigInteger.ONE;
+        return factorial(value - 1).multiply(BigInteger.valueOf(value));
+    }
+
+    /**
+     * Calculates factorial of given <code>value</code>.
+     * <p>Uses Stream and functional programming.
+     *
+     * @param value positive number
+     * @return factorial of <code>value</code>
+     */
+    public static BigInteger factorialStream(int value) {
+        return IntStream.rangeClosed(1, value)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.ONE, BigInteger::multiply);
     }
 }
